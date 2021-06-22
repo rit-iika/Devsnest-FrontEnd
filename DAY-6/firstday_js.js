586c24527a8519ccc11a787e1ad906338b074d32
Ritika <69005146+rit-iika@users.noreply.github.com>
2021-06-22 20:05:03 +0530

    Create firstday_js.js

---
+++ DAY-6/firstday_js.js
@@ -0,0 +1,37 @@
+
+//question 1
+function is_array(input){
+    ans=Array.isArray(input);
+    return ans;
+}
+console.log(is_array([1,2,4,0]));
+console.log(is_array('w3resource'));
+
+
+
+//question 4
+var myColor=["Red","Green","White","Black"];
+var joinit=myColor.join();
+console.log(joinit);
+var joinit=myColor.join(",");
+console.log(joinit);
+var joinit=myColor.join("+");
+console.log(joinit);
+
+//question 5
+var arr1=[3,'a','a','a',2,3,'a',3,'a',2,4,9,3];
+var mf=1;
+var m=0;
+var item;
+for(var i=0;i<arr1.length;i++){
+    for(var j=i;j<arr1.length;j++){
+        if(arr1[i]==arr1[j])
+        m++;
+        if(mf<m){
+            mf=m;
+            item=arr1[i];
+        }
+    }
+    m=0;
+}
+console.log(item+"("+mf+"times )");
