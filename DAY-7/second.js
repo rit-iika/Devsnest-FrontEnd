4093b5f17502bd61107056565b310e6ca1adbacf
Ritika <69005146+rit-iika@users.noreply.github.com>
2021-06-23 12:22:23 +0530

    Update second.js

--- DAY-7/second.js
+++ DAY-7/second.js
@@ -1,1 +1,10 @@
+//question 1
+ var student = { name : "David Rayy", sclass : "VI", rollno : 12 }
+ console.log(Object.keys(student));
 
+//question 2
+var student2 = { name : "David Rayy", sclass : "VI", rollno : 12 };
+console.log(student2);
+//to delete roll no 
+delete student2.rollno;
+console.log(student2);
