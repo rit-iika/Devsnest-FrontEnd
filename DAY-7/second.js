d6075571d1206c33ad89b9bb8340906a546e8304
Ritika <69005146+rit-iika@users.noreply.github.com>
2021-06-26 19:50:49 +0530

    Update second.js

--- DAY-7/second.js
+++ DAY-7/second.js
@@ -12,3 +12,9 @@ console.log(student2);
 //question 3
 var student3 = { name : "David Rayy", sclass : "VI", rollno : 12 }; 
 console.log(Object.keys(student3).length);
+
+//question 4
+var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }]; 
+console.log(Object.values(library[0]));
+console.log(Object.values(library[1]));
+console.log(Object.values(library[2]));
