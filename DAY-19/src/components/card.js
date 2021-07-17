65406a6c0e043a433e10ff4d915b4485457218b9
Ritika <69005146+rit-iika@users.noreply.github.com>
2021-07-17 11:29:43 +0530

    push counter

--- DAY-19/src/components/card.js
+++ DAY-19/src/components/card.js
@@ -1,1 +1,16 @@
+//using State hook ,which returns current state of varibale
+import { useState } from "react";
 
+var Box = () => {
+  var [count, setCount] = useState(0);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount((count += 1)); }}
+      >{count}</button>
+    </div>
+  );
+};
+
+export default Box;
