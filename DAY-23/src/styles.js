c0f3bcde81dc3efe0df75c4d38446daa40292071
Ritika <69005146+rit-iika@users.noreply.github.com>
2021-07-30 12:50:57 +0530

    react router

---
+++ DAY-23/src/styles.js
@@ -0,0 +1,42 @@
+.App {
+  font-family: sans-serif;
+  text-align: center;
+}
+.page {
+  background-color: lightgoldenrodyellow;
+  padding: 10px;
+}
+ul {
+  display: flex;
+  border: 2px solid black;
+  text-decoration: none;
+
+  /* flex-direction: right; */
+}
+nav li {
+  /* border: 2px solid red; */
+  /* background-color: lightblue; */
+  padding: 5px;
+  margin: 5px auto;
+  text-decoration: none;
+  list-style: none;
+  font-weight: bold;
+}
+h2 {
+  text-align: center;
+}
+.home {
+  border: 2px solid black;
+  padding: 20px;
+  background-color: aliceblue;
+}
+.about {
+  border: 2px solid black;
+  padding: 20px;
+  background-color: antiquewhite;
+}
+.users {
+  border: 2px solid black;
+  padding: 15px;
+  background-color: blanchedalmond;
+}
