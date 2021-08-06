3193c0b12681aef888b37c314c59a6018d7cc80f
Ritika <69005146+rit-iika@users.noreply.github.com>
2021-08-06 13:13:37 +0530

    todolist-react redux

---
+++ DAY-27/src/actions/index.js
@@ -0,0 +1,26 @@
+const addItem = (item) => {
+  return {
+    type: "ADD_ITEM",
+    payload: item
+  };
+};
+const remItem = (id) => {
+  return {
+    type: "REMOVE_ITEM",
+    payload: id
+  };
+};
+const loadTodo = () => {
+  return (dispatch) => {
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then((res) => res.json())
+      .then((todos) => {
+        return dispatch({
+          type: "LOAD_TODO",
+          payload: todos
+        });
+      });
+  };
+};
+
+export { addItem, remItem, loadTodo };
